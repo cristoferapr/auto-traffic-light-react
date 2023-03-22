4f21f7a506841356731511d89c2c7707bc2d138a
cristofer-apr <cpr.refor@gmail.com>
2023-03-22 13:21:29 +0000

    init

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,60 +6,41 @@ library.add(faClock)
 import rigoImage from "../../img/rigo-baby.jpg";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-const separate = (cont) => {
+const getnum = (cont) => {
 	var num;
-	if (cont / 60 >= 1){
-		cont = cont - (60 * Math.floor(cont / 60));
-		num= Math.floor((cont / 10 ));
+	if (cont >= 9){
+		cont = cont - (9 * Math.floor(cont / 9));
+		num= cont;
 	}else{
-		num= Math.floor((cont / 10 ));
+		num= cont;
 	}
 	return num;
 }
 
-const separate2 = (cont) => {
-	var num;
-	if (cont / 600 >= 1){
-		cont = cont - (600 * Math.floor(cont / 600));
-		num= Math.floor((cont / 60 ) );
+const green = (n) => {
+	if (n >=0 && n <3){
+		return " selected";
 	}else{
-		num= Math.floor((cont / 60 ) );
+		return '';
 	}
-	return num;
 }
 
-const separate3 = (cont) => {
-	var num;
-	if (cont / 3600 >= 1){
-		cont = cont - (3600 * Math.floor(cont / 3600));
-		num= Math.floor((cont / 600 ) );
+const yellow = (n) => {
+	if (n >=3 && n <6){
+		return " selected";
 	}else{
-		num= Math.floor((cont / 600 ) );
+		return '';
 	}
-	return num;
 }
 
-const separate4 = (cont) => {
-	var num;
-	if (cont / 36000 >= 1){
-		cont = cont - (36000 * Math.floor(cont / 36000));
-		num= Math.floor((cont / 3600 ) );
+const red = (n) => {
+	if (n >=6 && n <9){
+		return " selected";
 	}else{
-		num= Math.floor((cont / 3600 ) );
+		return '';
 	}
-	return num;
 }
 
-const separate5 = (cont) => {
-	var num;
-	if (cont / 360000 >= 1){
-		cont = cont - (360000 * Math.floor(cont / 360000));
-		num= Math.floor((cont / 36000 ) );
-	}else{
-		num= Math.floor((cont / 36000 ) );
-	}
-	return num;
-}
 
 //create your first component
 const Home = () => {
@@ -70,16 +51,15 @@ const Home = () => {
 		setCount(prevCount => prevCount + 1);
 	  }, 1000);
 	}, []);  
+	
 	return (
 		<div class="back">
 			<div class="container text-center" id="body">
-					<div class="digit"><FontAwesomeIcon icon="fa-regular fa-clock" /></div>
-					<div class="digit">{separate5(count)}</div>
-					<div class="digit">{separate4(count)}</div>
-					<div class="digit">{separate3(count)}</div>
-					<div class="digit">{separate2(count)}</div>
-					<div class="digit">{separate(count)}</div>
-					<div class="digit">{count % 10}</div>
+					<div class="light-container bg-dark">
+						<div className={"light green" + green(getnum(count))}></div>
+						</div>
+					<div class="light-container bg-dark"><div className={"light yellow" + yellow(getnum(count))}></div></div>
+					<div class="light-container bg-dark"><div className={"light red" + red(getnum(count))}></div></div>
 			</div>
             
 
